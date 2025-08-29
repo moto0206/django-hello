1ba85cf9f564cd48d348427510c2318cd8d87278
Naoki <motoyama00@synapse-i.jp>
2025-08-29 12:20:12 +0900

    Django HTMLテンプレートでHello Worldを表示する

--- helloWorld/views.py
+++ helloWorld/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+
+def say_hello(request):
+    return render(request, "helloWorld/hello.html", {"name": "Django"})
